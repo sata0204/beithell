038c3a09a2d3ea76bbaea8212736fa6ffe4d5f29
c01o <melty.antarcticite@gmail.com>
2014-12-04 21:47:47 +0900

    make folderinit.el barebone
    there should be bunch of bugs.

--- folderinit.el
+++ folderinit.el
@@ -1,31 +1,87 @@
 (defun beit-hell-test ()
-(interactive)
-("This is a test of the beithell.")
-;daigaku settei
-(setq is_kokuritsu (read-string "kokuritsu:1 shiritsu:2"))
-(setq univ_name (read-string "univ full name:"))
-(setq univ_short_name (read-string "univ short name:"))
-(setq has_only_college (read-string "tangakubu:1 others:2 :"))
-(if (equal has_only_college 1)
-  (setq college_option (read-string "zen:1 chu:2 kou:3 i:4 :"))
-  (setq college_name (read-string "college name:"))
-)
-;taimon settei
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq i 0)
-(while i < numbers_of_parts
-  ((push number_of_questions (read-string "number of questions:"))
-   (setq i (+ i 1)))
-)
-;folder name setting
-(setq univ_folder_path (read-string "number of parts:")) ;how to find TEX-Genkou dir?
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
-(setq numbers_of_parts (read-string "number of parts:"))
+  "This is a test of the beithell."
+  (interactive)
+  (let (;;local functions
+	(set-string-var (lambda (varname prompt) 
+			  (setq varname (read-string prompt))))
+	(set-numerical-var (lambda (varname prompt) 
+			     (setq varname (string-to-number (read-string prompt)))))
+	(set-upcased-string-var (lambda (varname prompt) 
+				  (setq varname (upcase-word (read-string prompt)))))
+	(set-downcased-string-var (lambda (varname prompt) 
+				    (setq varname (downcase-word (read-string prompt)))))
+	(set-capitalized-string-var (lambda (varname prompt) 
+				      (setq varname (capitalize-word (read-string prompt)))))
+	;;local variables
+	ritsu univ_name univ_short_name has_only_college college_option college_name 
+	numbers_of_parts i number_of_questions univ_folder_path univ_college_folder_name
+	college_folder_path suffixes_of_folders j suffix
+	)
+    (save-excursion
+      ;;大学基本設定
+      (funcall set-numerical-var 'ritsu "国立大なら1を、私立大なら2を入力")
+      (cond ((equal ritsu 1) (setq ritsu "kokuritsu"))
+	    ((equal ritsu 2) (setq ritsu "shiritsu"))
+	    (t (message "error-ritsu-value"))
+	    )
+      (funcall set-capitalized-string-var 'univ_name "大学名をローマ字12文字以内で入力")
+      (funcall set-upcased-string-var 'univ_short_name "大学略称をローマ字で入力")
 
+      ;;学部設定
+      (funcall set-numerical-var 'has_only_college "単学部なら1を、そうでないなら2を入力")
+      (cond ((equal has_only_college 1) 
+	     (progn 
+	       (funcall set-numerical-var 'college_option "前期なら1、中期なら2、後期なら3、医学部のみなら4を入力")
+	       (cond
+		((equal college_option 1) (setq college_name "zen"))
+		((equal college_option 2) (setq college_name "chu"))
+		((equal college_option 3) (setq college_name "ko"))
+		((equal college_option 4) (setq college_name "i"))
+		(t (message "error-college-option"))
+		)
+	       )
+	     )
+	    ((equal has_only_college 2) 
+	     (funcall set-downcased-string-var 'college_name "学部名を10字以内のローマ字で入力\n学科名は-で繋ぐこと\n(例：ri-oubutsu)")
+	     )
+	    (t (message "error-college-value"))
+	    )
+
+      ;;問題設定
+      (funcall set-numerical-var 'numbers_of_parts "大問数を入力")
+      (setq i 0)
+      (while (< i numbers_of_parts)
+	     (progn 
+	       (push number_of_questions (read-string (format "大問%dの小問数を入力" i)))
+	       (setq i (+ i 1))
+	       )
+	     )
+
+      ;;フォルダ名セット
+      (setq univ_folder_path (expand-file-name 
+			      (format "../TEX-Genkou/14-Nyushi/14-%c/14-%c" ritsu univ_name))
+	    )
+      (setq univ_college_folder_name (format "14-%c-%c" univ_short_name college_name))
+      (setq college_folder_path (format "%c/%c" univ_folder_path univ_college_folder_name))
+      ;;フォルダ名の語幹部と活用語尾部を別にセッティング
+      (setq suffixes_of_folders nil)
+      (setq j 0)
+      (setq k 0)
+      (while (< j numbers_of_parts)
+	((if (equal (elt number_of_questions j) 0) 
+	     (append suffixes_of_folders (list j))
+	   (while (< k (elt number_of_questions j))
+	     (append suffixes_of_folders (list (format "%d-%d" j k)));True
+	     );while k end
+	   )
+	 (setq j (+ j 1))
+	 );while j end
+	(append suffixes_of_folders (list "end" "matome"))
+	;;フォルダ実作成
+	(make-directory college_folder_path)
+	(dolist (suffix suffixes_of_folders)
+	  (make-directory (format "%s/%s-%s" college_folder_path univ_college_folder_name suffix))
+	  )
+	) ;save-excursion
+    ) ;let
+  ) ;defun
