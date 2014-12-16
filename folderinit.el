a6226e5d3d5be5c8f5a3dacbf67dd5ec40aabc6d
c01o <melty.antarcticite@gmail.com>
2014-12-16 20:34:35 +0900

    use error function and let special form.
    The code becomes more readable.

--- folderinit.el
+++ folderinit.el
@@ -14,15 +14,15 @@
 				      (set varname (capitalize (read-string prompt)))))
 	;;local variables
 	ritsu univ_name univ_short_name has_only_college college_option college_name 
-	numbers_of_parts i numbers_of_questions univ_folder_path univ_college_folder_name
-	college_folder_path suffixes_of_folders j suffix
+	numbers_of_parts numbers_of_questions univ_folder_path univ_college_folder_name
+	college_folder_path suffixes_of_folders suffix
 	)
     (save-excursion
       ;;大学基本設定
       (funcall set-numerical-var 'ritsu "国立大なら1を、私立大なら2を入力")
       (cond ((equal ritsu 1) (setq ritsu "kokuritsu"))
 	    ((equal ritsu 2) (setq ritsu "shiritsu"))
-	    (t (message "error-ritsu-value"))
+	    (t (error "なに立大学か不明。"))
 	    )
       (funcall set-capitalized-string-var 'univ_name "大学名をローマ字12文字以内で入力")
       (funcall set-upcased-string-var 'univ_short_name "大学略称をローマ字で入力")
@@ -37,26 +37,27 @@
 		((equal college_option 2) (setq college_name "chu"))
 		((equal college_option 3) (setq college_name "ko"))
 		((equal college_option 4) (setq college_name "i"))
-		(t (message "error-college-option"))
+		(t (error "何期大学か不明。"))
 		)
 	       )
 	     )
 	    ((equal has_only_college 2) 
 	     (funcall set-downcased-string-var 'college_name "学部名を10字以内のローマ字で入力\n学科名は-で繋ぐこと\n(例：ri-oubutsu)")
 	     )
-	    (t (message "error-college-value"))
+	    (t (error "何学部か不明。"))
 	    )
 
       ;;問題設定
       (funcall set-numerical-var 'numbers_of_parts "大問数を入力")
-      (setq i 0)
-      (while (< i numbers_of_parts)
-	(progn 
-	  (push 
-	   (string-to-number (read-string (format "大問%dの小問数を入力" (+ i 1)))) 
-	   numbers_of_questions
-	   )
-	  (setq i (+ i 1))
+      (let (i 0)
+	(while (< i numbers_of_parts)
+	  (progn 
+	    (push 
+	     (string-to-number (read-string (format "大問%dの小問数を入力" (+ i 1)))) 
+	     numbers_of_questions
+	     )
+	    (setq i (+ i 1))
+	    )
 	  )
 	)
       (setq numbers_of_questions (reverse numbers_of_questions))
@@ -69,18 +70,21 @@
       (setq college_folder_path (format "%s/%s" univ_folder_path univ_college_folder_name))
       ;;フォルダ名の語幹部と活用語尾部を別にセッティング
       (setq suffixes_of_folders nil)
-      (setq j 1)
-      (while (<= j numbers_of_parts)
-	(if (equal (elt numbers_of_questions (- j 1)) 0) 
-	    (setq suffixes_of_folders (append suffixes_of_folders (list j)))
-	  (setq k 1)
-	  (while (<= k (elt numbers_of_questions (- j 1)))
-	    (setq suffixes_of_folders (append suffixes_of_folders (list (format "%d-%d" j k))))
-	    (setq k (+ k 1))
-	    );while k end
-	  )
-	(setq j (+ j 1))
-	);while j end
+      (let  (j 1)
+	(while (<= j numbers_of_parts)
+	  (if (equal (elt numbers_of_questions (- j 1)) 0) 
+	      (setq suffixes_of_folders (append suffixes_of_folders (list j)))
+	    (let (k 1)
+	      (while (<= k (elt numbers_of_questions (- j 1)))
+		(setq suffixes_of_folders (append suffixes_of_folders (list (format "%d-%d" j k))))
+		(setq k (+ k 1))
+		);while k end
+	      );let k end
+	    )
+	  (setq j (+ j 1))
+	  );while j end
+	);let j end
+
       (setq suffixes_of_folders (append suffixes_of_folders (list "end" "matome")))
       ;;フォルダ実作成
       (make-directory college_folder_path 'recursive)
