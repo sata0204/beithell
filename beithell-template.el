7af228593470c40166f815e6010f4b4b32803d5a
c01o <melty.antarcticite@gmail.com>
2014-12-22 20:43:17 +0900

    Create main beithell.el / template

---
+++ beithell-template.el
@@ -0,0 +1,17 @@
+(defun beithell-funcname()
+  "Desrciption"
+  (interactive)
+  (let 
+      (
+       ;;local functions
+       ;;local variables
+       )
+    (save-excursion
+
+      ;;actual code
+
+      );save-excursion
+    );let
+  );defun
+
+  
---
+++ beithell.el
@@ -0,0 +1,17 @@
+;;; beithell.el --- make your tex works easy 
+
+;; Author/Maintainer:
+;;      c01o <melty.antarcticite@gmail.com>
+;; URL: http://github.com/c01o/beithell
+
+;;; License:NYSL Version 0.9982
+
+;; To obtain newest beithell, do
+;;      git clone git://github.com/c01o/beithell.git
+;;; Code:
+(require 'beithell-eps-input)
+(require 'beithell-folderinit)
+(require 'beithell-initializer)
+
+(provide 'beithell)
+;; code end
