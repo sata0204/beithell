e8f6a042f7b32ce32e9479bcd5508389359b3083
c01o <melty.antarcticite@gmail.com>
2014-12-23 00:26:14 +0900

    fix return value
    Convert windows style path, such as "C:\hoge\fuga"
    into / devided path(*), such as "C:/hoge/fuga".
    (*): if you evaluate the following S-exp in Windows,
    you'll get the same result.
    (expand-file-name "windows-style-path")

--- beithell-set-basepath.el
+++ beithell-set-basepath.el
@@ -3,13 +3,20 @@
   "read and set TEX-Genkou folder bath."
   (interactive)
   (progn
-    (defvar beithell-base-path)
+    (defvar beithell-basepath)
     (save-excursion
       (let ((true-path-p nil))
 	(while (not true-path-p)
-	  (setq beithell-base-path 
-		(read-string "TEX-Genkouの存在するパスを入力\n例：C:\\work\\TEX-Genkou\\14-Nyushi\\...なら\nC:\\work :"))
-	  (setq beithell-base-path (concat beithell-base-path "\\TEX-Genkou\\"))
-	  (setq true-path-p (yes-or-no-p (format "TEX-Genkouのパスは%sですか？" beithell-base-path)))
-	  )))))
-;;code end
+	  (setq beithell-basepath 
+		(read-string (append "TEX-Genkouの存在するパスを入力\n"
+				     "例：C:\\work\\TEX-Genkou\\14-Nyushi\\...なら\n"
+				     "C:\\work : ")) ;use append only for code indent
+		(setq beithell-basepath (concat beithell-basepath "\\TEX-Genkou\\"))
+		(setq true-path-p 
+		      (yes-or-no-p (format "TEX-Genkouのパスは%sですか？" beithell-basepath)))
+		);while end
+
+	  (setq beithell-basepath ;convert windows path into *nix-like path
+		(mapconcat 'identity (split-string beithell-basepath "\\\\") "/")) 
+	  ))))
+  ;;code end
