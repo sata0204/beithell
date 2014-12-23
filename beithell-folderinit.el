2a12992b3b2152435c904424e406bc8c5c63728e
c01o <melty.antarcticite@gmail.com>
2014-12-23 09:05:06 +0900

    fix paren mismatch bug

--- beithell-folderinit.el
+++ beithell-folderinit.el
@@ -48,61 +48,60 @@
 	(funcall set-downcased-string-var 'college_name 
 		 (append "学部名を10字以内のローマ字で入力\n" ;use append only for code indent 
 			 "学科名は-で繋ぐこと\n" 
-			 "(例：ri-oubutsu)"));cond=2 end
+			 "(例：ri-oubutsu)")));cond=2 end
 	(t (error "何学部か不明。"))
 	)
 
 
-      ;;大問設定
-      (funcall set-numerical-var 'numbers_of_parts "大問数を入力")
-      (setq parts_consist_of_questions
-	    (split-string 
-	     (funcall set-string-var 'parts_consist_of_questions 
-		      "小問集合問題である大問を入力\n半角数字、半角スペース区切り")))
-      ;;小問持ち大問のリストを数字リテラル化
-      (setq parts_consist_of_questions 
-	    (mapcar #'string-to-number parts_consist_of_questions)) 	  
-      ;;小問設定
-      (let ((i 0))
-	(while (< i numbers_of_parts)
-	  (progn 
-	    (push ;push (True:ask False:0) into numbers_of_questions
-	     (if (member (+ i 1) parts_consist_of_questions) 
-		 (string-to-number (read-string (format "大問%dの小問数を入力" (+ i 1)))) 0) 
-	     numbers_of_questions)
-	    (setq i (+ i 1))
-	    )));progn, while, let
-      (setq numbers_of_questions (reverse numbers_of_questions))
+       ;;大問設定
+       (funcall set-numerical-var 'numbers_of_parts "大問数を入力")
+       (setq parts_consist_of_questions
+	     (split-string 
+	      (funcall set-string-var 'parts_consist_of_questions 
+		       "小問集合問題である大問を入力\n半角数字、半角スペース区切り")))
+       ;;小問持ち大問のリストを数字リテラル化
+       (setq parts_consist_of_questions 
+	     (mapcar #'string-to-number parts_consist_of_questions)) 
+       ;;小問設定
+       (let ((i 0))
+	 (while (< i numbers_of_parts)
+	   (progn 
+	     (push ;push (True:ask False:0) into numbers_of_questions
+	      (if (member (+ i 1) parts_consist_of_questions) 
+		  (string-to-number (read-string (format "大問%dの小問数を入力" (+ i 1)))) 0) 
+	      numbers_of_questions)
+	     (setq i (+ i 1))
+	     )));progn, while, let
+       (setq numbers_of_questions (reverse numbers_of_questions))
 
 
-      ;;フォルダ名セット
-      (when (eq beithell-basepath nil) (beithell-set-basepath)) ;confirm basepath exsistence
-      (setq univ_folder_path 
-	    (expand-file-name (format "%s/14-Nyushi/14-%s/14-%s" 
-				      beithell-basepath ritsu univ_name)))
-      (setq univ_college_folder_name (format "14-%s-%s" univ_short_name college_name))
-      (setq college_folder_path (format "%s/%s" univ_folder_path univ_college_folder_name))
+       ;;フォルダ名セット
+       (when (eq beithell-basepath nil) (beithell-set-basepath)) ;confirm basepath exsistence
+       (setq univ_folder_path 
+	     (expand-file-name (format "%s/14-Nyushi/14-%s/14-%s" 
+				       beithell-basepath ritsu univ_name)))
+       (setq univ_college_folder_name (format "14-%s-%s" univ_short_name college_name))
+       (setq college_folder_path (format "%s/%s" univ_folder_path univ_college_folder_name))
 
-      ;;フォルダ名の問題番号部分を作成
-      (setq suffixes_of_folders nil)
-      (let  ((j 1))
-	(while (<= j numbers_of_parts)
-	  (if (equal (nthcdr (- j 1) numbers_of_questions) 0) 
-	      (add-to-list 'suffixes_of_folders (list j))
-	    (let ((k 1))
-	      (while (<= k (nthcdr (- j 1) numbers_of_questions))
-		(add-to-list 'suffixes_of_folders (list (format "%d-%d" j k)))
-		(setq k (+ k 1))));let k end
-	    );if end
-	  (setq j (+ j 1))));loop end
-      (add-to-list 'suffixes_of_folders (list "end" "matome")))
+       ;;フォルダ名の問題番号部分を作成
+       (setq suffixes_of_folders nil)
+       (let  ((j 1))
+	 (while (<= j numbers_of_parts)
+	   (if (equal (nthcdr (- j 1) numbers_of_questions) 0) 
+	       (add-to-list 'suffixes_of_folders (list j))
+	     (let ((k 1))
+	       (while (<= k (nthcdr (- j 1) numbers_of_questions))
+		 (add-to-list 'suffixes_of_folders (list (format "%d-%d" j k)))
+		 (setq k (+ k 1))));let k end
+	     );if end
+	   (setq j (+ j 1))));loop end
+       (add-to-list 'suffixes_of_folders (list "end" "matome")))
 
-    ;;フォルダ実作成
-    (make-directory college_folder_path 'recursive)
-    (dolist (suffix suffixes_of_folders)
-      (make-directory 
-       (format "%s/%s-%s" college_folder_path univ_college_folder_name suffix) 'recursive)
-      )
-    ) ;save-excursion
-  ) ;let
-) ;defun
+      ;;フォルダ実作成
+      (make-directory college_folder_path 'recursive)
+      (dolist (suffix suffixes_of_folders)
+	(make-directory 
+	 (format "%s/%s-%s" college_folder_path univ_college_folder_name suffix) 'recursive))
+      ) ;save-excursion
+    ) ;let
+  );defun
