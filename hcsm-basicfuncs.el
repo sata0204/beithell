461b082b009affd114952b42ea7e91a11a30723f
c01o <melty.antarcticite@gmail.com>
2014-12-26 10:49:33 +0900

    make folderinit actually works

--- hcsm-basicfuncs.el
+++ hcsm-basicfuncs.el
@@ -1,4 +1,4 @@
-(provide 'hcsm-basicfunc)
+(provide 'hcsm-basicfuncs)
 
 (defun hcsm-set-string-var(varname prompt &optional convert-option)
   "set `varname` from mini-buffer." ()
@@ -14,18 +14,14 @@
   (progn
     (defvar hcsm-basepath)
     (save-excursion
-      (let ((true-path-p nil))
-	(while (not true-path-p)
-	  (setq hcsm-basepath 
-		(read-string (append "TEX-Genkouの存在するパスを入力\n"
-				     "例：C:\\work\\TEX-Genkou\\14-Nyushi\\...なら\n"
-				     "C:\\work : ")) ;use append only for code indent
-		(setq hcsm-basepath (concat hcsm-basepath "\\TEX-Genkou\\"))
-		(setq true-path-p 
-		      (yes-or-no-p (format "TEX-Genkouのパスは%sですか？" hcsm-basepath)))
-		);while end
+      (let ((true-path-flag nil))
+	(while (not true-path-flag)
+	  (setq hcsm-basepath (concat (read-string "TEX-Genkouの存在するパスを入力\n例：C:\\work\\TEX-Genkou\\14-Nyushi\\...なら\nC:\\work : ") "\\TEX-Genkou"))
+	  (setq true-path-flag 
+		(yes-or-no-p (format "TEX-Genkouのパスは%sですか？" hcsm-basepath)))
+	  );while end
 
-	  (setq hcsm-basepath ;convert windows path into *nix-like path
-		(mapconcat 'identity (split-string hcsm-basepath "\\\\") "/")) 
+	(setq hcsm-basepath ;convert windows path into *nix-like path
+	      (mapconcat 'identity (split-string hcsm-basepath "\\\\") "/")) 
 	  ))))
   ;;code end
--- hcsm-folderinit.el
+++ hcsm-folderinit.el
@@ -1,26 +1,26 @@
 (provide 'hcsm-folderinit)
-(require 'hcsm-basicfunc)
+(require 'hcsm-basicfuncs)
 
 (defun hcsm-folderinit ()
   "Initialize folders."
   (interactive)
-  (let (;;local functions
-	;;local variables
+  (let (;;local variables
 	ritsu univ-name univ-short-name college-name 
 	      numbers-of-questions univ-folder-path univ-college-folder-name
 	      college-folder-path suffixes-of-folders
 	      )
     (save-excursion
       ;;大学基本設定
-      (setq ritsu hcsm-ask-ritsu)
+      (setq ritsu (hcsm-ask-ritsu))
       (hcsm-set-string-var 'univ-name "大学名をローマ字12文字以内で入力" 'capitalize)
       (hcsm-set-string-var 'univ-short-name "大学略称をローマ字で入力" 'upcase)
       ;;学部設定
-      (setq college-name hcsm-ask-collage)
+      (setq college-name (hcsm-ask-collage))
       ;;大問・小問設定
       (setq numbers-of-questions (hcsm-question-setting))
 
       ;;フォルダ名セット
+      (defvar hcsm-basepath nil)
       (when (eq hcsm-basepath nil) (hcsm-set-basepath)) ;confirm basepath exsistence
       (setq univ-folder-path 
 	    (expand-file-name (format "%s/14-Nyushi/14-%s/14-%s" 
@@ -28,7 +28,8 @@
       (setq univ-college-folder-name (format "14-%s-%s" univ-short-name college-name))
       (setq college-folder-path (format "%s/%s" univ-folder-path univ-college-folder-name))
       ;;フォルダ名の問題番号部分を作成
-      (setq suffixes-of-folders (hcsm-create-suffixes))
+      (setq suffixes-of-folders 
+	    (hcsm-create-suffixes (length numbers-of-questions) numbers-of-questions))
 
       ;;フォルダ実作成
       (hcsm-create-folders suffixes-of-folders college-folder-path univ-college-folder-name)
@@ -66,8 +67,8 @@
 	   ((equal college-option 4) (setq college-name "i"))
 	   (t (error "何期大学か不明。")))));cond=1 end
        ((equal has-only-college 2) 
-	(hcsm-set-string-var 'college-name ;use append only for code indent
-			     (append "学部名を10字以内のローマ字で入力\n"  
+	(hcsm-set-string-var 'college-name ;use concat only for code indent
+			     (concat "学部名を10字以内のローマ字で入力\n"  
 				     "学科名は-で繋ぐこと\n" 
 				     "(例：ri-oubutsu)") 'downcase));cond=2 end
        (t (error "何学部か不明。")));cond end
@@ -83,7 +84,7 @@
 	   (hcsm-set-string-var 'parts-consist-of-questions 
 				"小問集合問題である大問を入力\n半角数字、半角スペース区切り")))
     (setq parts-consist-of-questions ;リストの要素を文字から数字リテラルに変換
-	  (mapcar #'string-to-number parts-consist-of-questions)) 
+	  (mapcar #'string-to-number parts-consist-of-questions))
     ;;小問設定
     (let ((i 0))
       (while (< i numbers-of-parts)
@@ -95,7 +96,7 @@
 	  (setq i (+ i 1))
 	  )));progn, while, let
     (setq numbers-of-questions (reverse numbers-of-questions))
-    )))
+    ))
 
 (defun hcsm-create-folders(list-of-suffixes college-folder-path univ-college-folder-name)
   "create tex folders." ()
@@ -105,19 +106,20 @@
        (format "%s/%s-%s" college-folder-path univ-college-folder-name suffix) 
        'recursive))))
 
-(defun hcsm-create-suffixes()
+(defun hcsm-create-suffixes(numbers-of-parts numbers-of-questions)
   "create list of suffixes about parts and questions." ()
   (let (suffixes-of-folders)
     (save-excursion
       (setq suffixes-of-folders nil)
       (let  ((i 1))
 	(while (<= i numbers-of-parts)
-	  (if (equal (nthcdr (- i 1) numbers-of-questions) 0) 
-	      (add-to-list 'suffixes-of-folders (list i))
+	  (if (equal (nth (- i 1) numbers-of-questions) 0) 
+	      (add-to-list 'suffixes-of-folders (format "%d" i))
 	    (let ((j 1))
-	      (while (<= j (nthcdr (- i 1) numbers-of-questions))
-		(add-to-list 'suffixes-of-folders (list (format "%d-%d" i j)))
+	      (while (<= j (nth (- i 1) numbers-of-questions))
+		(add-to-list 'suffixes-of-folders (format "%d-%d" i j))
 		(setq j (+ j 1)))));let j, if end
 	  (setq i (+ i 1))));loop end
-      (add-to-list 'suffixes-of-folders (list "end" "matome")))
-    ))
+      (add-to-list 'suffixes-of-folders "end")
+      (add-to-list 'suffixes-of-folders "matome")
+      suffixes-of-folders)))
