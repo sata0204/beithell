7c350c1849c484a089d1c4ba97b048999c0d33b7
c01o <melty.antarcticite@gmail.com>
2014-12-27 10:42:44 +0900

    merge pull-request actually.

--- hcsm-eps-input.el
+++ hcsm-eps-input.el
@@ -1,56 +1,11 @@
 (provide 'hcsm-eps-input)
+(require 'hcsm-basicfuncs)
 ;;;run the function under the path like 
 ;;;/TEX-Genkou/14-kokuritsu/14-Daigaku/14-DIGK-gkb/14-DIGK-gkb-1/14-DIGK-gkb-toi-1.tex
-(defun hcsm-eps-input()
+(defun hcsm-eps-input (number)
   "Provides easy insertion of .eps pictures"
-  (interactive)
-  (let 
-      (current-path 
-       split-path 
-       split-actual-tex-path 
-       split-relative-tex-path 
-       tex-file-name
-       iterator-of-question
-       eps-file-name
-       split-relative-eps-path
-       relative-eps-path
-       formatted-eps-path
-       )
-    (save-excursion
-      ;;パス下ごしらえ
-      (setq current-path buffer-file-name)
-      (setq split-path (split-string current-path "/"))
-      ;;大学名から図名を取得
-      (setq split-actual-tex-path (member "TEX-Genkou" split-path))
-      (setq split-relative-tex-path (push "../../../../../.." split-actual-tex-path))
-      (setq tex-file-name (elt (last split-relative-tex-path) 0))
-      (setq iterator-of-question (read-string "図の番号は：")) ; will be replaced and automated 
-
-      (setq eps-file-name 
-	    (concat 
-	     (substring tex-file-name 0 -4) "-" iterator-of-question ".eps"
-	     )
-	    )
-
-      ;;epsのpathになるよう加工
-      (setq split-relative-eps-path (copy-sequence split-relative-tex-path))
-      (setf (elt 
-	     split-relative-eps-path 
-	     (- (length split-relative-eps-path) 1)
-	     ) 
-	    eps-file-name
-	    )
-      (setq relative-eps-path "")
-      (while split-relative-eps-path
-	(setq relative-eps-path 
-	      (concat relative-eps-path "/" (pop split-relative-eps-path))
-	)
-      )
-      ;;relative-eps-pathを整形して出力
-      (setq formatted-eps-path (concat "\\includegraphics[width=3.5cm]{" (substring relative-eps-path 1 nil) "}"))
-      (insert formatted-eps-path)
-      ) 
-    )
-  )
-
+  (interactive "n図の番号: ")
+  (hcsm-new-file (format "%s-%s.eps" (file-name-sans-extension (buffer-file-name) ) number)) ;該当epsが存在しないとき空ファイルを置くか聞く
+  (insert (format "\\includegraphics[width=3.5cm]{../../../../../../%s-%s}" (file-name-sans-extension (file-relative-name buffer-file-name hcsm-TEX-Genkou-position)) number)))
   
+;code end
