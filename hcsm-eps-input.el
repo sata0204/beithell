9fb8c0cbf8d50dfd838b6e62a5b86e1573c2eefc
c01o <melty.antarcticite@gmail.com>
2014-12-28 18:00:40 +0900

    close #15.
    solve Conflicts:
    	hcsm-basicfuncs.el
    	hcsm-eps-input.el

--- hcsm-eps-input.el
+++ hcsm-eps-input.el
@@ -2,10 +2,12 @@
 (require 'hcsm-basicfuncs)
 ;;;run the function under the path like 
 ;;;/TEX-Genkou/14-kokuritsu/14-Daigaku/14-DIGK-gkb/14-DIGK-gkb-1/14-DIGK-gkb-toi-1.tex
+
 (defun hcsm-eps-input (number)
   "Provides easy insertion of .eps pictures"
   (interactive "n図の番号: ")
-  (hcsm-new-file (format "%s-%s.eps" (file-name-sans-extension (buffer-file-name) ) number)) ;該当epsが存在しないとき空ファイルを置くか聞く
+  ;;the template should be in ~/.emacs.d/hcsm/templates/hcsm-empty.eps
+  ;; -> temp coding. should be fixed
+  (hcsm-ask-if-create-file (format "%s-%s.eps" (file-name-sans-extension (buffer-file-name)) number) "~/.emacs.d/hcsm/templates/hcsm-empty.eps")
   (insert (format "\\includegraphics[width=3.5cm]{../../../../../../%s-%s}" (file-name-sans-extension (file-relative-name buffer-file-name hcsm-TEX-Genkou-position)) number)))
-  
-;code end
+;;code end
