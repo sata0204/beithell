59ba509bbb2645494828f3c6e378a099183702fa
c01o <melty.antarcticite@gmail.com>
2014-12-29 22:36:46 +0900

    Fix #13
    assign prefix key to M-n

---
+++ hcsm-key-settings.el
@@ -0,0 +1,7 @@
+;; -*- coding: utf-8 -*-
+(provide 'hcsm-key-settings)
+
+;(global-set-key [] ')
+(global-set-key [?\M-n ?\C-f] 'hcsm-folderinit)
+(global-set-key [?\M-n ?\C-c] 'hcsm-setup)
+(global-set-key [?\M-n ?\C-e] 'hcsm-eps-input)
