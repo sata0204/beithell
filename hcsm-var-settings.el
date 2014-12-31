b73d087ea4c8196082d57134e7f5cd906e6e30d8
c01o <melty.antarcticite@gmail.com>
2014-12-31 17:17:38 +0900

    fix: bug for un-solved confrict

--- hcsm-var-settings.el
+++ hcsm-var-settings.el
@@ -1,4 +1,1 @@
-(provide 'hcsm-var-settings)
-(defvar hcsm-TEX-Genkou-path "~/.emacs.d/hocsom/TEX-Genkou")
-(defvar hcsm-school-year "15")
-(defvar hcsm-template-path "~/.emacs.d/hocsom/templates/")
+(provide `hcsm-var-settings)
