6f83c134611bc7e195f1d99b306ef9c9fba23e5b
sata0204 <sata0204@gmail.com>
2014-12-31 15:37:20 +0900

    Change the key settings
    M-c → M-s

--- hcsm-key-settings.el
+++ hcsm-key-settings.el
@@ -3,5 +3,5 @@
 
 ;(global-set-key [] ')
 (global-set-key [?\M-n ?\M-f] 'hcsm-folderinit)
-(global-set-key [?\M-n ?\M-c] 'hcsm-setup)
+(global-set-key [?\M-n ?\M-s] 'hcsm-setup)
 (global-set-key [?\M-n ?\M-e] 'hcsm-eps-input)
