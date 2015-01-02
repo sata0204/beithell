9e3116bbbde524b3f0ca8038f857cb7c4273be69
sata0204 <sata0204@gmail.com>
2015-01-02 20:22:36 +0900

    Change setup.el

--- hcsm-setup.el
+++ hcsm-setup.el
@@ -1,5 +1,6 @@
 (provide 'hcsm-setup)
 (require 'hcsm-basicfuncs)
+
 (defun hcsm-setup()
   "provide setting functions."
   (interactive)
@@ -9,11 +10,12 @@
     (hcsm-create-new-directory hcsm-TEX-Genkou-path)
 
     ;;hcsm-school-year
-    (hcsm-modify-settings 'hcsm-school-year (read-string "何年度?"))
+    (hcsm-modify-settings 'hcsm-school-year (format "%s/TEX-Genkou" (hcsm-get-school-year)))
 
     ;;hcsm-template-path
-    (hcsm-modify-settings 'hcsm-template-path (read-directory-name "template-path:"));temp
+;    (hcsm-modify-settings 'hcsm-template-path (read-directory-name "template-path:"));temp
     );save-excursion
+  (message "セットアップ完了")
   );defun
 
 
@@ -21,22 +23,23 @@
   "modify/generate setting written in `hcsm-var-settings.el`."
   (save-excursion 
     (if (boundp name) 
-	(when (y-or-n-p (format "%s の値を %s に変更しますか？" name value))
-	  (hcsm-open-to-kill "~/.emacs.d/hocsom/hcsm-var-settings.el"   ;; var `name` exists
-			     'hcsm-overwrite-defvar (list name value))) ;; -> modify setting
+	(hcsm-open-to-kill "~/.emacs.d/hocsom/hcsm-var-settings.el"   ;; var `name` exists
+			   'hcsm-overwrite-defvar (list name value)) ;; -> modify setting
       (hcsm-open-to-kill "~/.emacs.d/hocsom/hcsm-var-settings.el" ;; when `name` is void 
                          'hcsm-write-defvar (list name value)))   ;; -> create new setting
     (set name value)))
 
-(defun hcsm-get-TEX-Genkou-path()
+(defun hcsm-get-school-year ()
+  "set hcsm-school-year."
+  (let ((school-year (read-number "年度を入力(2015年度なら\"15\"): ")))
+    (if (y-or-n-p (format "20%s年度ですか?" school-year))
+	school-year
+      (hcsm-get-school-year))))
+
+(defun hcsm-get-TEX-Genkou-path ()
   "read and set TEX-Genkou folder bath."
-  (save-excursion
-    (defvar hcsm-TEX-Genkou-path) ;hcsm-TEX-Genkou-path should be void If you run the func.
-    (let (correct-path-flag)
-      (while (not correct-path-flag)
-	(setq hcsm-TEX-Genkou-path (format "%sTEX-Genkou" (read-directory-name "TEX-Genkouの存在するパスを入力\n例：C:\\work\\TEX-Genkou\\14-Nyushi\\...なら\nC:/work/ :\n例：~/work/TEX-Genkou/14-Nyushi/...なら\n~/work/ ")))
-	(setq correct-path-flag
-	      (y-or-n-p (format "TEX-Genkouのパスは%sですか？" hcsm-TEX-Genkou-path))))
-	hcsm-TEX-Genkou-path ;return it
-	)))
-;;code end
+  (let ((TEX-Genkou-path (read-directory-name "TEX-Genkouの存在するパス(フォルダ)を入力\n例：C:/work/TEX-Genkou/...ならC:/work/\n例：~/work/TEX-Genkou/...なら~/work/\n")))
+    (if (y-or-n-p (format "TEX-Genkouの存在するパス(フォルダ)は%sですか?" TEX-Genkou-path))
+	TEX-Genkou-path
+      (hcsm-get-TEX-Genkou-path))))
+
